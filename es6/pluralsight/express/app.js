869bacb070cb8ac76ccf73485cc8e8d2377665f4
Anthony Garo <agaro1121@gmail.com>
2018-02-05 23:43:36 -0500

    ads PUT, PATCH, DELETE, middleware

--- es6/pluralsight/express/app.js
+++ es6/pluralsight/express/app.js
@@ -27,6 +27,21 @@ bookRouter.route('/books')
     });
 
 /*
+* middleware
+* */
+bookRouter.use('/books/:bookId', (req, res, next) => {
+    console.log("You've reached middleware 1 !!!");
+    req.middleware1 = "middleware1_success";
+    next(); // if you don't call this, your api will hang!!!
+});
+
+bookRouter.use('/books/:bookId', (req, res, next) => {
+    console.log("You've reached middleware 2 !!!");
+    req.middleware2 = "middleware2_success";
+    next(); // if you don't call this, your api will hang!!!
+});
+
+/*
 * Simple get with url param
 * */
 bookRouter.route('/books/:bookId')
@@ -34,9 +49,37 @@ bookRouter.route('/books/:bookId')
         const bookId = req.params.bookId;
         const responseJson = {
             hello: "This my book api!",
-            ask: `You asked for bookId=${bookId}`
+            ask: `You asked for bookId=${bookId}`,
+            middleware_flag1: req.middleware1,
+            middleware_flag2: req.middleware2
         };
         res.json(responseJson);
+    })
+    .put((req, res) => {
+        const bookId = req.params.bookId;
+        const responseJson = {
+            hello: "This my book api!",
+            put: `You put bookId=${bookId}`,
+            payload: req.body,
+            middleware_flag1: req.middleware1,
+            middleware_flag2: req.middleware2
+        };
+        res.json(responseJson);
+    })
+    .patch((req, res) => {
+        const bookId = req.params.bookId;
+        const responseJson = {
+            hello: "This my book api!",
+            patch: `You patched bookId=${bookId}`,
+            payload: req.body,
+            middleware_flag1: req.middleware1,
+            middleware_flag2: req.middleware2
+        };
+        res.json(responseJson);
+    }).delete((req, res) => {
+        const bookId = req.params.bookId;
+        console.log(`You deleted bookId=${bookId}`);
+        res.status(204).send("Removed");
     });
 
 app.use("/api", bookRouter);
